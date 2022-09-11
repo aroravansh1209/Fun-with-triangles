b2241ab0209baca5d2d643a02bb356dd9b24efec
aroravansh1209 <109975931+aroravansh1209@users.noreply.github.com>
2022-09-12 02:16:16 +0530

    done some changes

--- area.js
+++ area.js
@@ -3,14 +3,14 @@ const button = document.querySelector("#area-of-triangle");
 const outputEl = document.querySelector("#output")
 
 function applyFormula(x,y){
-    const areaOfTriangle = x * y 
+    const areaOfTriangle = x * y
     return areaOfTriangle
 }
 
 function calculateAreaOfTriangle(){
     const areaOfTriangle = applyFormula(Number(sides[0].value),Number(sides[1].value));
     const finalArea = (areaOfTriangle)/2
-    outputEl.innerText = ("The Area Of Triangle Is : " + areaOfTriangle)
+    outputEl.innerText = ("The Area Of Triangle Is : " + finalArea)
 }
 
 button.addEventListener("click", calculateAreaOfTriangle)
