0ec369b8bbf9c07ef0eed12dbab354bd2071f5d4
aroravansh1209 <109975931+aroravansh1209@users.noreply.github.com>
2022-09-20 21:45:24 +0530

    added css

--- quiz.js
+++ quiz.js
@@ -2,19 +2,19 @@ const quizForm = document.querySelector(".quiz-form");
 const submitAnswerButton = document.querySelector("#submit-answer-button");
 const outputEl = document.querySelector("#output");
 
-const correctAnswers =["90°", "Right Angled"];
+const correctAnswers = ["90°", "Right Angled", "Yes", "Yes", "90°"];
 
-function calculateScore(){
-    let score = 0;
-    let index = 0;
-    const formResults = new FormData(quizForm);
-    for(let value of formResults.values()){
-        if(value === correctAnswers[index]){
-            score = score + 1;
-        }
-        index = index + 1;
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+  const formResults = new FormData(quizForm);
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
     }
-    outputEl.innerText = "Your Score is " + score;
+    index = index + 1;
+  }
+  outputEl.innerText = "Your Score is " + score;
 }
 
-submitAnswerButton.addEventListener("click",calculateScore)
+submitAnswerButton.addEventListener("click", calculateScore);
